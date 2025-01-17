d23354ed90f9f94834c50806c1fb89c55c6f0e32
cgbuen <christopherbuenaventura@gmail.com>
2025-01-17 14:37:04 -0800

    Fix email/mailto usage on commissions

--- src/routes/commissions/+page.svelte
+++ src/routes/commissions/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import { getContext } from 'svelte';
   import Card from '$lib/card.svelte';
-  import { PUBLIC_ASSET, PUBLIC_DISCORD_P, PUBLIC_INSTAGRAM, PUBLIC_EMAIL } from '$env/static/public';
+  import { PUBLIC_ASSET, PUBLIC_DISCORD_P, PUBLIC_INSTAGRAM, PUBLIC_MAILTO } from '$env/static/public';
 </script>
 
 <svelte:head>
@@ -18,7 +18,7 @@
     <div class="text dark:text-white text-neutral-900">
       Send me a message on Discord ({PUBLIC_DISCORD_P}) or 
       <a class="link-blank" href="{PUBLIC_INSTAGRAM}" target="_blank">Instagram</a>, or by 
-      <a class="link-blank" href="mailto:{PUBLIC_EMAIL}">email</a>.
+      <a class="link-blank" href="{PUBLIC_MAILTO}">email</a>.
     </div>
   </Card>
   <a class="pointer" href="https://forms.gle/tefHXyEh9WsAJjBs9" target="_blank">
@@ -39,7 +39,7 @@
     <div class="text dark:text-white text-neutral-900">
       Send me a message on Discord ({PUBLIC_DISCORD_P}), 
       <a class="link-blank" href="{PUBLIC_INSTAGRAM}" target="_blank">Instagram</a>, or 
-      <a class="link-blank" href="mailto:{PUBLIC_EMAIL}">email</a>.
+      <a class="link-blank" href="{PUBLIC_MAILTO}">email</a>.
     </div>
   </Card>
 </div>

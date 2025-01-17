6a733cf32288f3cb5f8da032a5cea52bcb0be892
cgbuen <christopherbuenaventura@gmail.com>
2025-01-17 14:04:23 -0800

    Add stubs for concerts and keyboards

---
+++ src/routes/concerts/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { getContext } from 'svelte';
+</script>
+
+<svelte:head>
+    <title>Concerts{getContext('titleSuffix')}</title>
+</svelte:head>
+
+<div class="concerts-container">
+  <h1>Concerts</h1>
+  <p>Under construction.</p>
+</div>
+
+<style>
+</style>
---
+++ src/routes/keyboards/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { getContext } from 'svelte';
+  import { page } from '$app/state';
+</script>
+
+<svelte:head>
+    <title>Commands &mdash; Resources{getContext('titleSuffix')}</title>
+</svelte:head>
+
+<div class="keyboards-container">
+  <h1>Keyboards</h1>
+  <p>Under construction.</p>
+</div>
+
+<style>
+</style>

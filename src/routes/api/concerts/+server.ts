8ea59c532dda8dd8750822c9d485819a2fcafe89
cgbuen <christopherbuenaventura@gmail.com>
2025-01-17 16:15:43 -0800

    Add concerts page
    - Add svelte-photoswipe-gallery
    - Configure on concerts page
    - (Required ordering change in sheet)

--- src/routes/api/concerts/+server.ts
+++ src/routes/api/concerts/+server.ts
@@ -1,44 +1,61 @@
-import { PUBLIC_ASSET } from "$env/static/public";
+import { PUBLIC_ASSET } from '$env/static/public';
 
 interface Photo {
-  active: boolean
-  city: string
-  date: string
-  height: number
-  origHeight: number
-  origWidth: number
-  roll: string
-  number: number
-  subject: string
-  venue: string
-  width: number
-  src: string
-  alt1: string
-  alt2: string
-  alt: string
-  description: string
+  active: boolean;
+  city: string;
+  date: string;
+  height: number;
+  origHeight: number;
+  origWidth: number;
+  roll: string;
+  number: number;
+  subject: string;
+  venue: string;
+  width: number;
+  src: string;
+  alt1: string;
+  alt2: string;
+  alt: string;
+  description: string;
 }
 
-const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const MONTHS = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
 
 export async function GET() {
-  const photographyResponse = await fetch(`${PUBLIC_ASSET}/concerts/concerts.json`)
-  const photographyResponseJson = await photographyResponse.json()
-  const response = photographyResponseJson
+  const photographyResponse = await fetch(`${PUBLIC_ASSET}/concerts/concerts.json`);
+  const photographyResponseJson = await photographyResponse.json();
+  let response = photographyResponseJson
     .filter((photo: Photo) => photo.active)
     .map((photo: Photo) => {
-      photo.src = `${PUBLIC_ASSET}/concerts/${photo.roll}+${photo.number}.jpg?${Date.now()}`
-      photo.origHeight = photo.height
-      photo.origWidth = photo.width
-      photo.alt1 = `${photo.subject}${(photo.venue && photo.venue.includes('n/a')) ? '' : ` @ ${photo.venue}`}`
-      photo.alt2 = photo.city === 'Coachella' ? `${photo.city} ${photo.date.substring(0, 4)}` : `${photo.city}, ${MONTHS[parseInt(photo.date.substring(5, 7)) - 1]} ${photo.date.substring(0, 4)}`
-      photo.alt = `${photo.alt1}, ${photo.alt2}`
-      photo.description = `${photo.alt1}\n${photo.alt2}`
-      return photo
-    })
+      photo.src = `${PUBLIC_ASSET}/concerts/${photo.roll}+${photo.number}.jpg?${Date.now()}`;
+      photo.origHeight = photo.height;
+      photo.origWidth = photo.width;
+      photo.alt1 = `${photo.subject}${photo.venue && photo.venue.includes('n/a') ? '' : ` @ ${photo.venue}`}`;
+      photo.alt2 =
+        photo.city === 'Coachella'
+          ? `${photo.city} ${photo.date.substring(0, 4)}`
+          : `${photo.city}, ${MONTHS[parseInt(photo.date.substring(5, 7)) - 1]} ${photo.date.substring(0, 4)}`;
+      photo.alt = `${photo.alt1}, ${photo.alt2}`;
+      photo.description = `${photo.alt1}\n${photo.alt2}`;
+      return photo;
+    });
+
   return new Response(JSON.stringify(response), {
-    headers : {
+    headers: {
       'Content-Type': 'application/json'
     }
-  })
+  });
 }
--- src/routes/concerts/+page.svelte
+++ src/routes/concerts/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   import { getContext } from 'svelte';
+  import { page } from '$app/state';
+  import PhotoswipeGallery from 'svelte-photoswipe-gallery'
+
+  const images = page.data.concerts
 </script>
 
 <svelte:head>
@@ -8,7 +12,13 @@
 
 <div class="concerts-container">
   <h1>Concerts</h1>
-  <p>Under construction.</p>
+  <p>Here are a few live music events that I&rsquo;m lucky to have shot. Click each image for more info.</p>
+  <PhotoswipeGallery
+    images={images}
+    showDescription={true}
+    showDownloadButton={false}
+    showFullscreenButton={false}
+  />
 </div>
 
 <style>
